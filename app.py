538a4e22f8db57b8832df79c7053d18b34c70513
natthawatkamkong-cyber <natthawatkamkong@gmail.com>
2026-05-25 20:16:14 +0700

    Refactor Telegram config and enhance webhook response logging
    Updated Telegram configuration and improved webhook logging.

--- app.py
+++ app.py
@@ -4,51 +4,70 @@ import requests
 app = Flask(__name__)
 
 # =========================
+# TELEGRAM CONFIG
+# =========================
+
+BOT_TOKEN = "8394186084:AAHeWDeMdEVwLuwdgNtm8YUuagxzV0QD0j0"
+
+# ห้อง VVIP GROUP
+CHAT_ID = "-100373230227"
+
+# =========================
 # HOME
 # =========================
+
 @app.route("/")
 def home():
     return "TVBOT ONLINE"
 
+# =========================
+# TEST PAGE
+# =========================
 
-# =========================
-# TEST
-# =========================
 @app.route("/test")
 def test():
     return "WEBHOOK TEST OK"
 
-
-# =========================
-# TELEGRAM CONFIG
-# =========================
-BOT_TOKEN = "8394186084:AAHeWDeMdEVwLuwdgNtm8YUuagxzV0QD0j0"
-CHAT_ID = "-100373230227"
-
-
 # =========================
 # WEBHOOK
 # =========================
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
 
     data = request.json
 
+    print("RAW DATA:", data)
+
     signal = data.get("signal", "")
     symbol = data.get("symbol", "")
+    tf = data.get("tf", "")
+
+    state = data.get("state", "")
+    bias_h1 = data.get("bias_h1", "")
+    bias_m15 = data.get("bias_m15", "")
+
     entry = data.get("entry", "")
     sl = data.get("sl", "")
+
     tp1 = data.get("tp1", "")
     tp2 = data.get("tp2", "")
     tp3 = data.get("tp3", "")
+
     rr = data.get("rr", "")
     session = data.get("session", "")
-    tf = data.get("tf", "")
+    time_value = data.get("time", "")
 
     message = f"""
 SIGNAL: {signal}
+
 SYMBOL: {symbol}
 TF: {tf}
+
+STATE: {state}
+
+BIAS_H1: {bias_h1}
+BIAS_M15: {bias_m15}
 
 ENTRY: {entry}
 SL: {sl}
@@ -58,7 +77,10 @@ TP2: {tp2}
 TP3: {tp3}
 
 RR: {rr}
+
 SESSION: {session}
+
+TIME: {time_value}
 """
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -71,14 +93,13 @@ SESSION: {session}
         }
     )
 
-    print(response.text)
+    print("TELEGRAM RESPONSE:", response.text)
 
     return {"status": "ok"}
-
 
 # =========================
 # RUN APP
 # =========================
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)
-
